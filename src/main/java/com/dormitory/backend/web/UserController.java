b1737c79c7dab051c6f7cfee066064a37a8d8f4c
etherealgemini <qqq1114174867@163.com>
2023-12-11 20:41:05 +0800

    Change:现在findDorm返回的宿舍将依次按location,buildingName,floor,houseNum，及指定的升降序排序。

--- src/main/java/com/dormitory/backend/web/UserController.java
+++ src/main/java/com/dormitory/backend/web/UserController.java
@@ -55,7 +55,9 @@ public class UserController {
     @PostMapping(value = "api/setComment")
     @Transactional
     @ResponseBody
-    public void setComment(@RequestBody comment object,@RequestParam String username,@RequestParam String dormitoryId,@RequestParam String content,@RequestParam Integer parentId){
+    public void setComment(@RequestBody comment object,@RequestParam String username,
+                           @RequestParam String dormitoryId,@RequestParam String content,
+                           @RequestParam Integer parentId){
         if (username==null||dormitoryId==null)
             throw new MyException(Code.MISSING_FIELD);
         userService.setComment(object,username,dormitoryId,content,parentId);
