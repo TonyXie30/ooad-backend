8726482dcfbde86bbb3fcf20cfd5693c7b5f59ab
etherealgemini <qqq1114174867@163.com>
2024-01-03 00:43:39 +0800

    FINAL

--- src/main/java/com/dormitory/backend/BackendApplication.java
+++ src/main/java/com/dormitory/backend/BackendApplication.java
@@ -5,15 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@EntityScan(basePackages = {"com.Dormitory.backend.pojo"})
+@EntityScan(basePackages = {"com.dormitory.backend.pojo"})
 @EnableTransactionManagement
 @SpringBootApplication
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
